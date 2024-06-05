78083f371721c901290b28efc326a14a2b2a7e96
Zhen Fan <zhenfan@babel-login-1.lti.cs.cmu.edu>
2024-06-05 13:41:07 -0400

    babel

--- scripts/train_dr_cw22b_noupdate.sh
+++ scripts/train_dr_cw22b_noupdate.sh
@@ -1,30 +1,27 @@
 #!/bin/bash
-#SBATCH --job-name=train_dr_openmatch
-#SBATCH --output=logs/%x-%j.out
-#SBATCH -e logs/%x-%j.err
-#SBATCH --partition=long
-#SBATCH --cpus-per-task=4
-#SBATCH --mem=200G
-#SBATCH --gres=gpu:A6000:4
-#SBATCH --time=7-00:00:00
-
+#SBATCH --cpus-per-task=8
+#SBATCH --mem=120G
+#SBATCH --gres=gpu:A6000:2
+#SBATCH --time=1-00:00:00
+module load cuda-12.1
 eval "$(conda shell.bash hook)"
 conda activate openmatch
 
 DATA_PATH="."
 
 text_length=2048
-n_gpus=4
+n_gpus=2
 
-num_episodes=4
+export OPENMATCH_DIR="/home/zhenfan/t5-rope-2048/AnchorDR/OpenMatch/src/openmatch/"
 
 train_qrels=$DATA_PATH/data/training_data/train.qrels.tsv
 train_queries=$DATA_PATH/data/training_data/train.query.txt
 corpus=$DATA_PATH/data/training_data/corpus_cw22btrain.tsv
 negatives=$DATA_PATH/data/training_data/train.negatives.tsv
 
-initial_model=yiqingx/AnchorDR
-trained_model_name=AnchorDR-cw22b-$text_length
+initial_model=jmvcoelho/t5-base-marco-crop-pretrain-2048
+
+trained_model_name=t5rope-cw22b-$text_length-noupdate-5ep
 
 train_data_folder=$DATA_PATH/data/training_data/$trained_model_name
 mkdir -p $train_data_folder
@@ -32,7 +29,7 @@ mkdir -p $train_data_folder
 echo "########################################"
 echo "Building initial data"
 echo "########################################"
-
+'''
 python OpenMatch/scripts/msmarco/build_train.py \
    --tokenizer_name $initial_model \
    --negative_file $negatives  \
@@ -57,8 +54,8 @@ echo $n_train
 tail -n $n_val $train_data_folder/full.jsonl > $train_data_folder/val.jsonl
 head -n $n_train $train_data_folder/full.jsonl > $train_data_folder/train.jsonl
 
-rm $train_data_folder/full.jsonl
-
+#rm $train_data_folder/full.jsonl
+'''
 echo "########################################"
 echo "Train episodes"
 echo "########################################"
@@ -68,8 +65,7 @@ valid_data=$train_data_folder/val.jsonl
 output_path=$DATA_PATH/models/$trained_model_name
 
 
-
-accelerate launch --num_processes $n_gpus --multi_gpu --main_process_port 29777 OpenMatch/src/openmatch/driver/train_dr.py  \
+accelerate launch --num_processes $n_gpus --multi_gpu --main_process_port 29989 OpenMatch/src/openmatch/driver/train_dr.py  \
     --output_dir $output_path \
     --model_name_or_path $initial_model \
     --do_train \
@@ -84,36 +80,36 @@ accelerate launch --num_processes $n_gpus --multi_gpu --main_process_port 29777 
     --learning_rate 5e-6  \
     --q_max_len 32  \
     --p_max_len $text_length  \
-    --num_train_epochs 5  \
+    --num_train_epochs 2  \
     --report_to wandb \
     --logging_steps 10 \
     --run_name $trained_model_name \
     --evaluation_strategy steps \
-    --dataloader_num_workers 4 \
+    --dataloader_num_workers 8 \
     --rope True \
     --grad_cache True \
     --use_mapping_dataset True \
     --gc_p_chunk_size 24 \
     --gc_q_chunk_size 24 \
     --negatives_x_device True 
-    
-    # Hard negative sampling - ANCE negative refresh
-    # set variables for next training episode
 
-    initial_model=$output_path
+# Hard negative sampling - ANCE negative refresh
+# set variables for next training episode
 
-    embeddings_out=$DATA_PATH/data/embeddings/train/$trained_model_name
-    run_save=$DATA_PATH/data/negatives/$trained_model_name
+initial_model=$output_path
 
-    trained_model_name=$trained_model_name-self-hn-$i
-    train_data_folder=$DATA_PATH/data/training_data/$trained_model_name
+embeddings_out=$DATA_PATH/data/embeddings/train/$trained_model_name
+run_save=$DATA_PATH/data/negatives/$trained_model_name
 
-    mkdir -p $run_save
-    mkdir -p $embeddings_out
+trained_model_name=$trained_model_name-self-hn-$i
+train_data_folder=$DATA_PATH/data/training_data/$trained_model_name
 
-    # Train again with the hard negatives - don't set the variable for the last one
-    if [ $i -ne $num_episodes ]; then
-        train_data=$train_data_folder/train.jsonl
-        valid_data=$train_data_folder/val.jsonl
-        output_path=$DATA_PATH/models/marco/$trained_model_name
-    fi
+mkdir -p $run_save
+mkdir -p $embeddings_out
+
+# Train again with the hard negatives - do not set the variable for the last one
+if [ $i -ne $num_episodes ]; then
+    train_data=$train_data_folder/train.jsonl
+    valid_data=$train_data_folder/val.jsonl
+    output_path=$DATA_PATH/models/marco/$trained_model_name
+fi
